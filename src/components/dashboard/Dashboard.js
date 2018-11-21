0528c6ef7d649b0cb93230731fdeee6de27812e0
EsmerlinJM <20164379@itla.edu.do>
2018-11-20 21:07:56 -0400

    Add notifications and deploy app on Firebase Hosting

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -6,7 +6,7 @@ import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
 class Dashboard extends Component {
     render() {
-        const { posts } = this.props 
+        const { posts, notifications } = this.props 
         return (
             <div className="dashboard container">
                 <div className="row">
@@ -14,7 +14,7 @@ class Dashboard extends Component {
                         <PostList posts={posts}/>
                     </div>
                     <div className="col s12 m5 offset-m1">
-                        <Notifications />
+                        <Notifications notifications={notifications} />
                     </div>
                 </div>
             </div>
@@ -28,13 +28,15 @@ class Dashboard extends Component {
  */
 const mapStateToProps = (state) => {
     return {
-        posts: state.firestore.ordered.posts
+        posts: state.firestore.ordered.posts,
+        notifications: state.firestore.ordered.notifications
     }
 } 
 
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        { collection: 'posts' }
+        { collection: 'posts', orderBy: ['createdAt', 'desc'] },
+        { collection: 'notifications', limit: 3, orderBy: ['time', 'desc'] }
     ])
 )(Dashboard)
